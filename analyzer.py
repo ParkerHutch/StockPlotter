5a95b4df4c0c52be5f074f7454fba7f4a1c2be1e
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-21 12:00:36 -0500

    cleanup

--- analyzer.py
+++ analyzer.py
@@ -21,14 +21,13 @@ def process_data(dataset: pd.DataFrame) -> pd.DataFrame:
         dataset['Close/Last'].shift(1), dataset['Close/Last'].shift(0))
     
 
-def clean_data(dataset: pd.DataFrame) -> pd.DataFrame:
+def clean_data(dataset: pd.DataFrame):
     """Prepares the dataset in-place for processing. This step includes sorting
     the dataset by date, dropping the index, and cleaning column headers.
 
     Args:
         dataset (pd.DataFrame): a pandas DataFrame object representing the 
         raw data from the NASDAQ API
-
     """
 
     dataset.reset_index(drop=True, inplace=True)
@@ -60,16 +59,20 @@ def output_stock_info(ticker: str, dataset: pd.DataFrame):
     
     max_percent_change_row = dataset.iloc[[dataset['Percent Change'].argmax()]]
     min_percent_change_row = dataset.iloc[[dataset['Percent Change'].argmin()]]     
+    
     print(f'Max % Change: {max_percent_change_row["Percent Change"].item():.2f}%', 
         f'({max_percent_change_row["Date"].item().date().strftime("%m/%d/%Y")})')
     print(f'Min % Change: {min_percent_change_row["Percent Change"].item():.2f}%',
         f'({min_percent_change_row["Date"].item().date().strftime("%m/%d/%Y")})')
+    
     max_price_row = dataset.iloc[[dataset['High'].argmax()]]
+    min_price_row = dataset.iloc[[dataset['Low'].argmin()]]
+
     print(f'High: ${max_price_row["High"].item():.2f}',
         f'({max_price_row["Date"].item().date().strftime("%m/%d/%Y")})')
-    min_price_row = dataset.iloc[[dataset['Low'].argmin()]]
     print(f'Low: ${min_price_row["Low"].item():.2f}',
         f'({min_price_row["Date"].item().date().strftime("%m/%d/%Y")})')
+    
     price_range = abs(max_price_row["High"].item() - 
                         min_price_row["Low"].item())
     print(f'Range: ${price_range:.2f}')
